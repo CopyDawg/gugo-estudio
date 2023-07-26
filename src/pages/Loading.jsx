639c96e5aaf01ba054b5f9c822a9fbea54df3b7f
Omar <omarcory2@gmail.com>
2023-07-26 12:23:05 -0600

    FIX IMAGES SRC

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,15 +1,18 @@
 
+import apple_logo from '../assets/apple-logo.png';
+import dot_icon from '../assets/icons/dot.png';
+
 export const Loading = () => {
   return (
     <div className="loader-wrapper">
         <div className="loader-logo spin">
-            <img src="src\assets\apple-logo.png" alt="Logo"/>
+            <img src={apple_logo} alt="Logo"/>
         </div>
         <div className="loader-message">
             <h3>Loading</h3>
-            <img src="src\assets\icons\dot.png" className="bounce-loader" />
-            <img src="src\assets\icons\dot.png" className="bounce-loader animation--delay-200" />
-            <img src="src\assets\icons\dot.png" className="bounce-loader animation--delay-400" />
+            <img src={dot_icon} className="bounce-loader" />
+            <img src={dot_icon} className="bounce-loader animation--delay-200" />
+            <img src={dot_icon} className="bounce-loader animation--delay-400" />
         </div>
         <p className="loader-disclaimer">Page made with ❤️</p>
     </div>
